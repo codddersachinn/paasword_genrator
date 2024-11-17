4b5d8f95121b87b4f47db0aff6fa8cccfecd9264
saccchinnnn <guptaasachin87@gmail.com>
2024-11-17 16:03:05 +0530

    comite

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = (props) => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="content has-text-centered">
+                    <h3>Created By Sachin </h3>
+                    <p className="field">
+                        
+                        
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
